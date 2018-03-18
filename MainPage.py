a673a3a8cb8766662b7510c8abd507c3052a1bb1
vinitathakur <vinita.thakur789@gmail.com>
2018-03-18 14:58:31 -0700

    Added pickle part

--- MainPage.py
+++ MainPage.py
@@ -13,9 +13,11 @@ def first_page():
 @app.route("/result", methods=["GET","POST"])
 def result_page():
     doc_data = request.form["doc_data"]
-    inverted_index = inverted_indexing.create_inverted_index()
-    with open("my_index.txt", "wb")as my_index:
-        pickle.dump(inverted_index, my_index)
+    # inverted_index = inverted_indexing.create_inverted_index()
+    # with open("my_index.txt", "wb")as my_index:
+    #     pickle.dump(inverted_index, my_index)
+    with open("my_index.txt", "rb") as myFile:
+        inverted_index = pickle.load(myFile)
     label, confidence = inverted_indexing.get_doc_label(inverted_index, doc_data.strip())
     return str(label+", "+str(confidence))
 
